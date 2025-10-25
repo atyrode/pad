2da9b3dd8153effdc5c3d4a96f281d63afb5d867
Alex TYRODE <alex@tyrode.dev>
2025-10-25 23:41:35 +0200

    refactor: add DEFAULT_SIZE constant to RACK_CONSTANTS and update initial rack size in store to use this constant

--- src/constants/rack.ts
+++ src/constants/rack.ts
@@ -11,6 +11,9 @@ export const RACK_CONSTANTS = {
   
   // Rack container margin (in pixels)
   RACK_MARGIN: 0,
+
+  // Default rack size
+  DEFAULT_SIZE: 7,
 } as const;
 
 /**
--- src/game/store.tsx
+++ src/game/store.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { createContext, useContext, useReducer } from "react";
 import { BOARD_CONSTANTS } from "../constants/board";
+import { RACK_CONSTANTS } from "../constants/rack";
 
 export type TileModel = {
   id: string;
@@ -26,7 +27,6 @@ type Action =
   | { type: "SET_BOARD_DIMS"; width: number; height: number }
   | { type: "SET_RACK_SIZE"; size: number };
 
-const DEFAULT_RACK = 7;
 
 function makeBoard(
   width: number,
@@ -45,7 +45,7 @@ const initialState: GameState = {
   boardWidth: BOARD_CONSTANTS.DEFAULT_WIDTH,
   boardHeight: BOARD_CONSTANTS.DEFAULT_HEIGHT,
   board: makeBoard(BOARD_CONSTANTS.DEFAULT_WIDTH, BOARD_CONSTANTS.DEFAULT_HEIGHT),
-  rack: Array.from({ length: DEFAULT_RACK }).map(() => null),
+  rack: Array.from({ length: RACK_CONSTANTS.DEFAULT_SIZE }).map(() => null),
 };
 
 function reducer(state: GameState, action: Action): GameState {
